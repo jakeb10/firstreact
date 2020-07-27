ebbcc2e43785c0f90fd3c773485674fde13ef8b5
Jake Barker <rjbarker@vt.edu>
2020-07-27 16:21:38 -0400

    Updates

---
+++ precache-manifest.399d37522db8a25034a64efb485be7ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc23037813a502c1690adcb2624a5771",
+    "url": "/firstreact/index.html"
+  },
+  {
+    "revision": "cbfc31d014abdfbd1944",
+    "url": "/firstreact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1df2b748d7afe2ed022",
+    "url": "/firstreact/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "cbfc31d014abdfbd1944",
+    "url": "/firstreact/static/js/2.fa4b67ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/firstreact/static/js/2.fa4b67ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1df2b748d7afe2ed022",
+    "url": "/firstreact/static/js/main.9842cf4a.chunk.js"
+  },
+  {
+    "revision": "c0a8c71a63c218ae4de5",
+    "url": "/firstreact/static/js/runtime-main.376334cc.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/firstreact/static/media/mintbean.cf18b629.png"
+  }
+]);
